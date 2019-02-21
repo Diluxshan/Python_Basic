61d76b0a5a214c9818460104c4cba70dfe6c70ea
Diluxshan <diluxv@gmail.com>
2019-02-21 11:31:23 +0530

    Basic Codes testing for python beginners.

---
+++ Python/Codes/Basic_test/Basic1.py
@@ -0,0 +1,58 @@
+
+#1 Argument pass with parameter.
+def val(d):
+   print(d)
+
+   # # print('Enter the name:')
+   # na=input('Enter the name:')
+   # print('Welcome '+na)
+
+val('\n1. name')
+
+#============================================
+
+
+#2 class verification
+class clas():
+    def fun(self):
+        print('\n2. Hello...Im CAR')
+        print('2. This is non Static Method..')
+#============================================
+
+
+#3 Staticmethod
+class Static():
+    @staticmethod
+    def stat():
+        print('\n3. This is Static Method...')
+#============================================
+
+
+#4 Inheritance
+class version1():
+    @staticmethod
+    def v1():
+        print('\n4. This is Version 1..(inheritance)')
+        print('4. This is Version 1-copied..(inheritance)')
+
+class version2(version1):
+    def v2(self):
+        print('\n4. This is Version 2..-NonStatic(inheritance)')
+
+class version3(version2):
+    def v3(self):
+        print('\n4. This is Version 3..-NonStatic(inheritance)')
+#============================================
+
+
+
+if __name__ == '__main__':
+    obj=clas()
+    obj.fun()
+
+    Static.stat()
+
+    version2.v1()
+
+    inherit = version2()
+    inherit.v2()
