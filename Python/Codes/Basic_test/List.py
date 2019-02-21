9217bcfc998009359a734cf575adb6d6f6d97b4c
Diluxshan <diluxv@gmail.com>
2019-02-21 15:01:25 +0530

    Array append & extend

--- Python/Codes/Basic_test/List.py
+++ Python/Codes/Basic_test/List.py
@@ -1,6 +1,7 @@
 print("It's List and Array in Python...!")
 
 arr = ['A','B','C','D','E']
+num = ['10','20','30','40','50']
 
 print(arr[2])
 print(arr[2:])
@@ -8,3 +9,7 @@ print(arr[2:])
 # it should be dont take 4th element.
 print(arr[2:4])
 
+arr.extend(num)
+arr.append('extra adding')
+
+print(arr)
