e3f9ef898a23bd303c0a48f04cc994fb0a1b1f3a
Diluxshan <diluxv@gmail.com>
2019-03-16 14:45:33 +0530

    print statement change

--- Python/Codes/Flower Care/bluetooth connection.py
+++ Python/Codes/Flower Care/bluetooth connection.py
@@ -6,12 +6,11 @@ time=str(datetime.datetime.today())
 
 def do_something(sc):
     print("\nWakeup..!")
-    print ("Doing stuff...")
+    print ("Doing stuff one...")
     print(time)
     # do your stuff
-    s.enter(60, 60, do_something, (sc,))
+    s.enter(2, 2, do_something, (sc,))
     print("Going to sleep")
 
 s.enter(5, 5, do_something, (s,))
 s.run()
-d.
